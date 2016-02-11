e1cadced8d5c12d782ba4bb327e235affc0855f3
bbouley <b.bouley@hotmail.co.uk>
2016-02-10 18:48:05 -0700

    basic server set up, sending back poke api

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var request = require('request');
+
+function handleRequest(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    request('http://pokeapi.co/api/v2/pokemon/1', function(err, response, body) {
+        if(err) {
+            res.end('Error, Request Failed - ' + err);
+        } else if (!err && response.statusCode === 200) {
+            res.end(body);
+        }
+    });
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(8080, 'localhost', function() {
+    console.log('listening on port 8080');
+});
